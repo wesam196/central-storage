8b98ca29e5e2f74dd833a98c13986fa15f93e533
wesam <wesam.a.s196@gmail.com>
2024-03-24 10:59:04 +0300

    add bootstrap to home screen with navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,50 @@
 <head>
 
 <title>Home File</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-primary" id="nav_background">
+  <div class="container-fluid">
+    <div class="navbar-brand">
+      <img src="images\cropped-masar_logo-1-1-60x86.png" alt="logo">
+    </div>
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link text-white" href="/">Home </a>
+        <a class="nav-item nav-link text-white" href="{{url('/upload')}}">upload</a>
+        <a class="nav-item nav-link text-white" href="{{url('/upload')}}">files shred to me</a>
+        <a class="nav-item nav-link text-white" href="{{url('/upload')}}">files shred to other</a>
+
+   
+        @if (Route::has('login'))
+        @auth
+        <x-app-layout>
+        </x-app-layout>
+        @else
+        <a href="{{ route('register') }}" class="nav-item nav-link text-white">Register</a>
+        <a href="{{ route('login') }}" class="nav-item nav-link text-white">Login</a>
+        @endauth
+        @endif
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+
     <h1>welcome {{Auth::user()->name}}</h1>
     <h1>The App work well!!</h1>
     <h1>no errors</h1>
@@ -26,7 +66,7 @@
         <tr>
             <th>{{$file->filename}}</th>
             <th><input type="checkbox" name='assigned[]' value={{$file->id}} ></th>
-            <th><a href="{{ url('/download/' . $file->id) }}">{{$file->id}}</a></th>
+            <th><a href="{{ url('/download/' . $file->id) }}" class='btn'>download</a></th>
            
         </tr>
         @endforeach
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,7 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <x-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
+       
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
